9b2fcae4c8cf290bb074f101574d8f91057e7c6b
Руслан Академия <ruslan.oficial@mail.ru>
2025-04-03 09:45:40 +0300

    1-коммит

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+	"restapi/model"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("restApi.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Ошибка подкллючения к БД")
+	}
+	db.AutoMigrate(&model.User{}, &model.Chat{}, model.Message{})
+
+}
+
+/*go get -u github.com/gin-gonic/gin
+go get -u gorm.io/gorm
+go get -u gorm.io/driver/sqlite
+go get -u github.com/joho/godotenv
+*/
--- model/model.go
+++ model/model.go
@@ -1,4 +1,24 @@
 package model
 
+import "gorm.io/gorm"
+
 type User struct {
+	gorm.Model
+	Name  string
+	Pass  int
+	Chats []Chat `gorm:"foreignKey:UserID"`
+}
+
+type Chat struct {
+	gorm.Model
+	UserID   int
+	Messages []Message `gorm:"foreignKey:ChatID"`
+}
+
+type Message struct {
+	gorm.Model
+	ChatID    int
+	Content   string
+	Role      string
+	SeachType string
 }
